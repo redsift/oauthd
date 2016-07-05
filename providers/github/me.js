a0b96e6b8b9ebbb904eda1557166758d53c9b845
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2016-07-06 00:01:38 +0100

    github /me now aggregates from /user/emails and /user API calls

--- providers/github/me.js
+++ providers/github/me.js
@@ -1,19 +1,68 @@
-var me = {
+'use strict';
+/* jshint camelcase: false */
 
-    url: '/user',
-    params: {},
-    fields: {
-        id: function(me) {
-            return "" + me.id;
-        },
-        name: '=',
-        company: '=',
-        alias: 'login',
-        bio: '=',
-        avatar: 'avatar_url',
-        email: '=',
-        location: '='
+var me = {
+  fetch: [
+    {
+      url: '/user/emails',
+      export: {
+        emails: function (result) {
+          console.log('email=', result);
+          return result;
+        }
+      }
+    },
+    {
+      url: '/user',
+      export: {
+        user: function (result) {
+          console.log('user=', result);
+          return result;
+        }
+      }
+    },
+    function (fetched_elts) {
+      console.log('fetched_elts=', fetched_elts);
+      return fetched_elts;
     }
+  ],
+  params: {},
+  fields: {
+    id: function (me) {
+      return '' + me.user.id;
+    },
+    name: function (me) {
+      return me.user.name;
+    },
+    company: function (me) {
+      return me.user.company;
+    },
+    alias: function (me) {
+      return me.user.login;
+    },
+    bio: function (me) {
+      return me.user.bio;
+    },
+    avatar: function (me) {
+      return me.user.avatar_url;
+    },
+    email: function (me) {
+      var email = null;
+      me.emails.forEach(function (em) {
+        console.log('email=', em, em.primary && em.verified);
+        if(em.primary === true && em.verified === true) {
+          email = em.email;
+        }
+      });
+      return email;
+    },
+    emails: function (me) {
+      return me.emails;
+    },
+    location: function (me) {
+      return me.user.location;
+    }
+  }
 };
 
 module.exports = me;
