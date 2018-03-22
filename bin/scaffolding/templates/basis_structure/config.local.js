5cde8a66fff428ecce7903c4634be58110b13d8f
Christos Vontas <christos@redsift.io>
2018-03-22 15:04:03 +0000

    removed bin folder [skip ci]

--- bin/scaffolding/templates/basis_structure/config.local.js
+++
@@ -1,8 +0,0 @@
-var local_config = {
-	// Here you can override any configuration field that is specific to a local machine.
-	// The config.local.js is git ignored.
-
-};
-
-
-module.exports = local_config;
